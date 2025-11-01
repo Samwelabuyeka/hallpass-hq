13446aa35a8b10794250aa55ffd97edcdcfc2f58
samwelabuyeka90 <samwelabuyeka90@gmail.com>
2025-11-01 12:05:05 +0000

    feat: Troubleshoot and fix dependency issues

--- src/main.tsx
+++ src/main.tsx
@@ -2,29 +2,4 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
-// Remove Lovable badge
-const removeLovableBadge = () => {
-  const observer = new MutationObserver(() => {
-    const badge = document.querySelector('[data-lovable-badge]');
-    if (badge) {
-      badge.remove();
-    }
-  });
-  
-  observer.observe(document.body, {
-    childList: true,
-    subtree: true
-  });
-  
-  // Also remove on load
-  setTimeout(() => {
-    const badge = document.querySelector('[data-lovable-badge]');
-    if (badge) {
-      badge.remove();
-    }
-  }, 1000);
-};
-
-removeLovableBadge();
-
 createRoot(document.getElementById("root")!).render(<App />);
